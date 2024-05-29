791b40e53bfa2ed5c2b4bc7e4bd5a8256ad6eb69
Oleksandr Kulkov <hutofsato@gmail.com>
2024-05-29 13:48:36 +0300

    Added an article

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -15,7 +15,7 @@ export function formatDate(date: any) {
 }
       
 export function exchangePrice(price: number) {
-    const courseRate = 41;
+    const courseRate = 41.5;
     const coursePrice = courseRate * price;
     return coursePrice.toFixed();
 }
